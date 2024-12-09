905530fb09643fcf23d01adf110d29786c406152
David Meguira <davidmgr93@gmail.com>
2024-12-09 02:35:18 +0100

    amélioration front des vues + utilisation du paramètre de logout pour afficher un message de déco

--- www/Views/front.php
+++ www/Views/front.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title><?php echo $title??"Titre de ma page";?></title>
-        <meta name="description" content="<?php echo $description??"Ceci est la description de la page";?>">
-    </head>
-    <body>
-        <h1>Template du front</h1>
-        <?php include $this->view;?>
-    </body>
-</html>
 
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title ?? "Titre de ma page"; ?></title>
+    <meta name="description" content="<?php echo $description ?? "Ceci est la description de la page"; ?>">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+        }
+
+        a {
+            color: blue;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Template du front</h1>
+    <?php include $this->view; ?>
+</body>
+
+</html>
